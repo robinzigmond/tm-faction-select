896d134ecfa879d3c5010632cfb6020969d5ec6e
Robin Zigmond <robinzig@hotmail.com>
2017-12-30 23:57:16 +0000

    added round scoring tiles selection. Would like to not display the (unused) cult bonus for the round 6 scoring tile

--- src/App.js
+++ src/App.js
@@ -24,6 +24,27 @@ const bonusTiles = [
    "description": "3 VP for each shipping level when passing, income 3 power"}
 ]
 
+const scoringTiles = [
+  {"id": 1,
+   "description": "2VP/Dwelling, 1P per 4 Water"},
+  {"id": 2,
+   "description": "2VP/Dwelling, 4PW per 4 Fire"},
+  {"id": 3,
+   "description": "3VP/TP, 1 spade per 4 Air"},
+  {"id": 4,
+   "description": "3VP/TP, 1 spade per 4 Water"},
+  {"id": 5,
+   "description": "5VP per SH/SA, 1W per 2 Air"},
+  {"id": 6,
+   "description": "5VP per SH/SA, 1W per 2 Fire"},
+  {"id": 7,
+   "description": "2VP/spade, 1C per 1 Earth"},
+  {"id": 8,
+   "description": "5VP/TW, 1 spade per 4 Earth"},
+  {"id": 9,
+   "description": "4VP/TE, 2 coins per cult priest"}
+]
+
 class App extends Component {
   render() {
     return (
@@ -38,14 +59,16 @@ class App extends Component {
 class FactionPicker extends Component {
   constructor(props) {
     super(props);
-    const taken = [1, 2, 3, 4, 5, 6, 7].map(num => ({"no": num, "val": 0}));
-    this.state = {"numPlayers": 4, "taken": taken};
+    const bonTaken = [1, 2, 3, 4, 5, 6, 7].map(num => ({"no": num, "val": 0}));
+    const scoreTaken = [1, 2, 3, 4, 5, 6].map(num => ({"no": num, "val": 0}));
+    this.state = {"numPlayers": 4, "bonusesTaken": bonTaken, "scoringTilesTaken": scoreTaken};
     this.handleNumPlayersChange = this.handleNumPlayersChange.bind(this);
     this.handleBonusSelectionChange = this.handleBonusSelectionChange.bind(this);
+    this.handleScoringSelectionChange = this.handleScoringSelectionChange.bind(this);
   }
 
   handleNumPlayersChange(event) {
-    var taken = this.state.taken.slice();
+    var taken = this.state.bonusesTaken.slice();
     while (taken.length < +event.target.value + 3) {
       let currentNum = taken[taken.length-1].no;
       taken.push({"no": currentNum+1, "val": 0})
@@ -56,11 +79,11 @@ class FactionPicker extends Component {
         newTaken.push(taken[i]);
       }
     }
-    this.setState({"numPlayers": +event.target.value, "taken": newTaken});
+    this.setState({"numPlayers": +event.target.value, "bonusesTaken": newTaken});
   }
 
   handleBonusSelectionChange(event) {
-    var taken = this.state.taken.slice();
+    var taken = this.state.bonusesTaken.slice();
     for (let i=1; i<=this.state.numPlayers + 3; i++) {
       if (event.target.name == "bonusTileNo" + i) {
         for (let j=0; j<taken.length; j++) {
@@ -70,15 +93,31 @@ class FactionPicker extends Component {
         }
       }
     }
-    this.setState({"taken": taken});
+    this.setState({"bonusesTaken": taken});
+  }
+
+  handleScoringSelectionChange(event) {
+    var taken = this.state.scoringTilesTaken.slice();
+    for (let i=1; i<=6; i++) {
+      if (event.target.name == "scoringTileRound" + i) {
+        for (let j=0; j<taken.length; j++) {
+          if (taken[j].no == i) {
+            taken[j].val = +event.target.value;
+          }
+        }
+      }
+    }
+    this.setState({"scoringTilesTaken": taken});    
   }
 
   render() {
     return (
       <form>
         <Select type="numPlayers" value={this.state.numPlayers} changeHandler={this.handleNumPlayersChange}/>
-        <BonusTileSelects num={this.state.numPlayers + 3} alreadySelected={this.state.taken}
+        <BonusTileSelects num={this.state.numPlayers + 3} alreadySelected={this.state.bonusesTaken}
         changeHandler={this.handleBonusSelectionChange}/>
+        <ScoringTileSelects alreadySelected={this.state.scoringTilesTaken}
+        changeHandler={this.handleScoringSelectionChange}/>
       </form>
     );
   }
@@ -90,13 +129,25 @@ class BonusTileSelects extends Component {
     for (let i=0; i<this.props.num; i++) {
       neededTilesArray.push(i);
     }
-    console.log(this.props.alreadySelected.length);
     return (
       neededTilesArray.map((num) => <Select type="bonusTiles" key={""+num} name={"bonusTileNo"+(num+1)}
                                     identity={num+1}
                                     changeHandler={this.props.changeHandler}
                                     alreadySelected={this.props.alreadySelected}
                                     value={this.props.alreadySelected.filter(obj => obj.no==num+1)[0].val}/>)
+    );
+  }
+}
+
+class ScoringTileSelects extends Component {
+  render() {
+    var neededTilesArray = [0,1,2,3,4,5];
+    return (
+      neededTilesArray.map((num) => <Select type="scoringTiles" key={""+num} name={"scoringTileRound"+(num+1)}
+                                     identity={num+1}
+                                     changeHandler={this.props.changeHandler}
+                                     alreadySelected={this.props.alreadySelected}
+                                     value={this.props.alreadySelected.filter(obj => obj.no==num+1)[0].val}/>)
     );
   }
 }
@@ -110,10 +161,15 @@ class Select extends Component {
       keys = ["2", "3", "4", "5"];
 
     }
-    if (this.props.type == "bonusTiles") {
+    else if (this.props.type == "bonusTiles") {
       label = "Bonus tile";
       options = bonusTiles;
       keys = bonusTiles.map((tile) => tile.id);
+    }
+    else if (this.props.type == "scoringTiles") {
+      label = "Scoring tile for Round " + this.props.identity;
+      options = scoringTiles;
+      keys = scoringTiles.map((tile) => tile.id);
     }
     return (
       <div>
@@ -135,11 +191,16 @@ class OptionList extends Component {
     const options = this.props.options;
     const identity = this.props.identity;
     function isOptionOK(option) {
+      // eliminate selections already taken by others in the same section
       for (let i=0; i<alreadySelected.length; i++) {
         if (alreadySelected[i].val == option.id && alreadySelected[i].val > 0
         && alreadySelected[i].no != identity) {
           return false;
         }
+      }
+      // eliminate spade scoring tile from round 5 or 6
+      if (options == scoringTiles && identity > 4 && option.id == 7) {
+        return false;
       }
       return true;
     }
